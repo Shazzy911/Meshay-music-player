3f27a1ac20f42a51c7b5c422f9d92b7970fd4644
Shazzy911 <Shazzydev911@gmail.com>
2024-07-31 12:23:15 +0500

    31st july update changing slide info

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,10 @@ const nextConfig = {
           protocol: 'https',
           hostname: 'img.freepik.com',
         },
+        {
+          protocol: 'https',
+          hostname: 'plus.unsplash.com',
+        },
         // You can add more patterns for additional domains if needed
       ],
     },
--- src/json/slide_dummy.tsx
+++ src/json/slide_dummy.tsx
@@ -12,7 +12,8 @@ export const slide_dummy: slide_dummy_types[] = [
     
     {
         id: 2,
-        image: "https://img.freepik.com/premium-photo/happy-young-girl-podcast-studio_74981-275.jpg?w=826",
+        image: "https://images.unsplash.com/photo-1502773860571-211a597d6e4b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjJ8fG11c2ljfGVufDB8fDB8fHww",
+        
         heading: "The New Ultimate Music Experience",
         description: "Immerse yourself in the world of music with our live shows and documentaries! From sold-out concerts to intimate acoustic sessions, our live shows bring the energy and excitement of live music straight to your device.",
         btn1: "Learn More",
@@ -20,20 +21,30 @@ export const slide_dummy: slide_dummy_types[] = [
     },
     {
         id: 3,
-        image: "https://img.freepik.com/free-photo/front-view-man-playing-guitar-indoors_23-2149849434.jpg?t=st=1716023876~exp=1716027476~hmac=adbf33d23a5328e81ed2459becf4fee84be9d5b3e8c7995ddff3f163c5e2243b&w=826",
+        image: "https://img.freepik.com/premium-photo/happy-young-girl-podcast-studio_74981-275.jpg?w=826",
+        
         heading: "Upcoming Events You Won't Want to Miss!",
         description: "Stay ahead of the curve with the latest music releases and upcoming events! Our 'New Releases' section is updated daily with fresh drops from top artists and emerging talent. Get ready to stream the hottest new tracks and albums as soon as they're available.",
         btn1: "Explore Premium",
     },
     {
         id: 4,
-        image: "https://img.freepik.com/premium-photo/happy-woman-with-arms-raised_1048944-11342580.jpg?w=826",
+        image: "https://images.unsplash.com/photo-1598488035139-bdbb2231ce04?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTV8fG11c2ljfGVufDB8fDB8fHww",
+        
         heading: "Stay Ahead of the Beat: Unlock the Power of Music Discovery",
         description: "Get ready to uncover new sounds and artists with our cutting-edge music discovery features! Our algorithm-driven recommendations learn your listening habits and serve up fresh tracks, playlists, and radio stations tailored just for you.",
         btn1: "Buy Now",
         btn2: "New Release",
     }
-
+    ,
+    {
+        id: 5,
+        image: "https://images.unsplash.com/photo-1501612780327-45045538702b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fG11c2ljfGVufDB8fDB8fHww",
+        heading: "Stay Ahead of the Beat: Unlock the Power of Music Discovery",
+        description: "Get ready to uncover new sounds and artists with our cutting-edge music discovery features! Our algorithm-driven recommendations learn your listening habits and serve up fresh tracks, playlists, and radio stations tailored just for you.",
+        btn1: "Buy Now",
+        btn2: "New Release",
+    }
     // https://images.pexels.com/photos/625644/pexels-photo-625644.jpeg?auto=compress&cs=tinysrgb&w=1000
 
 ]
