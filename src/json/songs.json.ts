a689a521c952367a694df223d94969d88f0deff4
shazzy911 <shazzy.mern@gmail.com>
2025-01-02 17:07:47 +0500

    feat: using use-sound library to play audio integration successfully done

--- src/json/songs.json.ts
+++ src/json/songs.json.ts
@@ -11,21 +11,21 @@ type SongDataType = {
 export const songs: SongDataType[] = [
   {
     id: 1,
-    title: "Shape of You",
-    featuring: "Ed Sheeran",
+    title: "Fingertips",
+    featuring: "Lana Del Rey",
     image:
       "https://i.pinimg.com/236x/f9/d0/70/f9d070ab175d5817deed4f688e1e868f.jpg",
-    file: "https://zckzxwurwibtoteccdta.supabase.co/storage/v1/object/public/music-store/audio/Fingertips.mp3?t=2024-12-31T18%3A09%3A27.033Z",
+    file: "https://zckzxwurwibtoteccdta.supabase.co/storage/v1/object/public/music-store/audio/Fingertips.mp3?t=2025-01-02T12%3A00%3A54.249Z",
     time: "3:45",
     album: "Divide",
   },
   {
     id: 2,
-    title: "Blinding Lights",
-    featuring: "The Weeknd",
+    title: "Paris Texas",
+    featuring: "Lana del rey feat (SYML)",
     image:
-      "https://i.pinimg.com/236x/f9/d0/70/f9d070ab175d5817deed4f688e1e868f.jpg",
-    file: "https://zckzxwurwibtoteccdta.supabase.co/storage/v1/object/public/music-store/audio/Fingertips.mp3?t=2024-12-31T18%3A09%3A27.033Z",
+      "https://i.pinimg.com/236x/f8/b9/14/f8b914d17ec1154b51458ef6c0cd9822.jpg",
+    file: "https://zckzxwurwibtoteccdta.supabase.co/storage/v1/object/public/music-store/audio/Paris,%20Texas%20(feat.%20SYML).mp3?t=2025-01-02T12%3A01%3A39.900Z",
     time: "3:20",
     album: "After Hours",
   },
